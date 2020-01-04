dd490521652b199a5d68982984ff89d2c3575cf8
shehio <shehabyasser@gmail.com>
2020-01-04 15:07:25 -0800

    Added MDP class.

---
+++ markovdecisionprocess.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class MarkovDecisionProcess:
+
+    def __init__(self, state_count):
+        self.state_count = state_count
+        self.states = np.array(self.state_count)
+
+
+
+
+
+
+
